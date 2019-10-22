e6224ecf32029f926c7a5b52cddcdcfa5cd6a0e8
SubrataSharma <sharmasubrata45@gmail.com>
2019-10-23 02:10:03 +0530

    second map/ mechanic dashbord 25

--- app/src/main/java/com/example/maptesttwoapplication/ShowBuyerDataActivity.java
+++ app/src/main/java/com/example/maptesttwoapplication/ShowBuyerDataActivity.java
@@ -2,7 +2,9 @@ package com.example.maptesttwoapplication;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.text.HtmlCompat;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
@@ -31,7 +33,8 @@ import java.util.Objects;
 
 public class ShowBuyerDataActivity extends AppCompatActivity {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private TextView seller_price_data,seller_data;
+    private TextView buyer_price_data,buyer_product_name_address
+            ,product_detail,seller_detail;
     private ImageView product_image;
     private ProgressBar progressBar;
     private String sellerPhoneNO = " ";
@@ -40,6 +43,10 @@ public class ShowBuyerDataActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_buyer_data);
         product_image = findViewById(R.id.buyer_product_image_show);
+        buyer_price_data = findViewById(R.id.buyer_price_data_show);
+        buyer_product_name_address = findViewById(R.id.buyer_product_name_address_show);
+        product_detail = findViewById(R.id.product_detail_data_show);
+        seller_detail = findViewById(R.id.seller_data_show);
         progressBar =findViewById(R.id.progress_circular);
         progressBar.setVisibility(View.VISIBLE);
         String buyerId = Objects.requireNonNull(getIntent().getExtras()).getString("buyerID");
@@ -52,26 +59,50 @@ public class ShowBuyerDataActivity extends AppCompatActivity {
                         assert documentSnapshot != null;
                         SellerData sellerData = documentSnapshot.toObject(SellerData.class);
                         assert sellerData != null;
-                        Glide.with(ShowBuyerDataActivity.this)
-                                .load(sellerData.getFileUrl())
-                                .listener(new RequestListener<Drawable>() {
-                                    @Override
-                                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
-                                        progressBar.setVisibility(View.INVISIBLE);
-                                        return false;
-                                    }
-
-                                    @Override
-                                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
-                                        progressBar.setVisibility(View.INVISIBLE);
-                                        return false;
-
-                                    }
-                                }).into(product_image);
-
                         sellerPhoneNO = sellerData.getContactNo();
+                        dataLoadingForBuyer(sellerData);
                     }
                 });
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void dataLoadingForBuyer(SellerData sellerData) {
+        assert sellerData != null;
+        Glide.with(ShowBuyerDataActivity.this)
+                .load(sellerData.getFileUrl())
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        progressBar.setVisibility(View.INVISIBLE);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        progressBar.setVisibility(View.INVISIBLE);
+                        return false;
+
+                    }
+                }).into(product_image);
+        buyer_price_data.setText("₹ "+sellerData.getSellingPrice());
+        if(sellerData.getSellingType().equals("wholesaling")){
+        String nameAddress=" " + sellerData.getProductName() + " <br>"+
+                "for <b>" +sellerData.getSellingType() + "</b><br> "
+                + " ☞ " + sellerData.getContactDetail();
+        buyer_product_name_address.setText(HtmlCompat.fromHtml(nameAddress, HtmlCompat.FROM_HTML_MODE_COMPACT)
+                , TextView.BufferType.SPANNABLE);
+        }else {
+            String nameAddress=" " + sellerData.getProductName() + " <br>"
+                    + " ☞ " + sellerData.getContactDetail();
+            buyer_product_name_address.setText(HtmlCompat.fromHtml(nameAddress, HtmlCompat.FROM_HTML_MODE_COMPACT)
+                    , TextView.BufferType.SPANNABLE);
+        }
+        product_detail.setText(sellerData.getProductDetail());
+        String sellerDetail="<i>name</i> : "+ sellerData.getSellerName() + " <br>"
+                + "\uD83D\uDCF1 +0 " + sellerData.getContactNo();
+        seller_detail.setText(HtmlCompat.fromHtml(sellerDetail, HtmlCompat.FROM_HTML_MODE_COMPACT)
+                , TextView.BufferType.SPANNABLE);
 
     }
 
